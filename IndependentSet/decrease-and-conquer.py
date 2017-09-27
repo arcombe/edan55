1b6b55dc4e81fd74ef54fc904cabb80c6241527a
Arcombe <dat13aar@dtudent.lu.se>
2017-09-27 13:31:43 +0200

    Done but slow

--- IndependentSet/decrease-and-conquer.py
+++ IndependentSet/decrease-and-conquer.py
@@ -1,13 +1,15 @@
 import scipy as scy
 import pylab as py
 import random as rd
+import sys
 import math as ma
 import numpy as np
+import copy
 
 class decreaseAndConquer:
     
-    def __init__(self):
-        f = open('C:\\Users\\anton\\Code\\g4.in', 'r')
+    def __init__2(self, file):
+        f = open(file, 'r')
         self.n = int(f.readline())
         self.temp = []
         for x in range(0,self.n):
@@ -17,7 +19,62 @@ class decreaseAndConquer:
         print(self.m)
         print(self.r0(self.m))
             
-    def r0(self, graph):
+    def __init__(self, file):
+        f = open(file, 'r')
+        self.n = int(f.readline())
+        self.G = [[],[]]
+        for x in range(self.n):
+            self.G[0].append(x)
+            tmp = f.readline()
+            e = []
+            for y in range(self.n):
+                if tmp[y*2] == '1':
+                    e.append(y)
+            self.G[1].append(e)
+        
+    def __call__(self):
+        res = self.r0(self.G)
+        return res
+    
+    def __remove_v(self, v, G, neighbours = 0):
+        node = G[0][v]
+        nb = G[1][v]
+        del G[0][v]
+        del G[1][v]
+        for x in range(len(nb)):
+            n = G[0].index(nb[x])
+            G[1][n].remove(node)
+            if neighbours == 1:
+                G = self.__remove_v(n, G)
+        return G
+        
+    
+    def r0(self, G):
+        size = len(G[0])
+        if size != 0:
+            max = 0
+            v = 0
+            for x in range(size):
+                s = len(G[1][x])
+                if s == 0:
+                    G = self.__remove_v(x, G)
+                    return 1 + self.r0(G)
+                elif s > max:
+                    max = s
+                    v = x
+            G_copy = copy.deepcopy(G)
+            G1 = self.__remove_v(v, G_copy)
+            G2 = self.__remove_v(v, G, neighbours = 1)
+            res1 = self.r0(G1)
+            res2 = 1 + self.r0(G2)
+            if res1 > res2:
+                return res1
+            else:
+                return res2
+        else:
+            return 0
+        
+    def r0_2(self, graph):
         print('Calling r0...')
         self.m = graph
         self.oldSum = 0
@@ -42,6 +99,10 @@ class decreaseAndConquer:
         else:
             return 0 
         
+        
 if __name__ == "__main__":
-    "dac = decreaseAndConquer()"
-    
+    dac = decreaseAndConquer(sys.argv[1])
+    res = dac()
+    print(res)
+    #x = [[1,2,3], [[2,3],[1,3], [1,2]]]
+    #print(x[1][0])
