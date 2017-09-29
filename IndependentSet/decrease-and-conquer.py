d819d0564a71af48543441ed5f55a62341222b17
Arcombe <dat13aar@dtudent.lu.se>
2017-09-29 12:19:10 +0200

    Done

--- IndependentSet/decrease-and-conquer.py
+++ IndependentSet/decrease-and-conquer.py
@@ -2,60 +2,46 @@ import scipy as scy
 import pylab as py
 import random as rd
 import sys
-import math as ma
+import math
 import numpy as np
 import copy
 
 class decreaseAndConquer:
-    
-    def __init__2(self, file):
-        f = open(file, 'r')
-        self.n = int(f.readline())
-        self.temp = []
-        for x in range(0,self.n):
-            self.temp.append(f.readline().split())
-        self.m = np.matrix(self.temp, dtype=int)
-        self.m.reshape(self.n,self.n)
-        print(self.m)
-        print(self.r0(self.m))
             
     def __init__(self, file):
         f = open(file, 'r')
         self.n = int(f.readline())
-        self.G = [[],[]]
+        self.G = dict()
+        self.iterations = 0
         for x in range(self.n):
-            self.G[0].append(x)
             tmp = f.readline()
             e = []
             for y in range(self.n):
                 if tmp[y*2] == '1':
                     e.append(y)
-            self.G[1].append(e)
-        
+            self.G[x] = e
+            
     def __call__(self):
-        res = self.r0(self.G)
-        return res
+        res = self.r2(self.G)
+        return res, self.iterations
     
     def __remove_v(self, v, G, neighbours = 0):
-        node = G[0][v]
-        nb = G[1][v]
-        del G[0][v]
-        del G[1][v]
-        for x in range(len(nb)):
-            n = G[0].index(nb[x])
-            G[1][n].remove(node)
+        nb = G[v]
+        del G[v]        
+        for x in nb:
+            G[x].remove(v)
             if neighbours == 1:
-                G = self.__remove_v(n, G)
+                G = self.__remove_v(x, G)     
         return G
-        
-    
+            
     def r0(self, G):
-        size = len(G[0])
+        size = len(G)
+        self.iterations += 1
         if size != 0:
             max = 0
             v = 0
-            for x in range(size):
-                s = len(G[1][x])
+            for x in G:
+                s = len(G[x])
                 if s == 0:
                     G = self.__remove_v(x, G)
                     return 1 + self.r0(G)
@@ -72,37 +58,84 @@ class decreaseAndConquer:
             else:
                 return res2
         else:
+            return 0        
+    
+    def r1(self, G):
+        size = len(G)
+        self.iterations += 1
+        if size != 0:
+            max = 0
+            v = 0
+            for x in G:
+                s = len(G[x])
+                if s == 0:
+                    G = self.__remove_v(x, G)
+                    return 1 + self.r1(G)
+                elif s == 1:
+                    G = self.__remove_v(x, G, neighbours = 1)
+                    return 1 + self.r1(G)
+                elif s > max:
+                    max = s
+                    v = x
+            G_copy = copy.deepcopy(G)
+            G1 = self.__remove_v(v, G_copy)
+            G2 = self.__remove_v(v, G, neighbours = 1)
+            res1 = self.r1(G1)
+            res2 = 1 + self.r1(G2)
+            if res1 > res2:
+                return res1
+            else:
+                return res2
+        else:
             return 0
         
-    def r0_2(self, graph):
-        print('Calling r0...')
-        self.m = graph
-        self.oldSum = 0
-        if self.m.size != 0:
-            for x in range(0,self.n):
-                self.sum = self.m[x].sum()
-                if self.sum == 0:
-                    self.temp = self.m[x]
-                    self.m.remove(self.m[x])
-                    return 1 + self.r0(self.m)
-            for x in range(0,self.n):
-                self.sum = self.m[x].sum()
-                if self.sum > self.oldSum:
-                    self.index = x
-                    self.oldSum = self.sum
-            self.leave = self.m.remove(self.m[self.index])
-            self.toRemove = self.m[self.index].nonzero()
-            for i in range(0,self.toRemove.size()):
-                self.m = self.m.delete(self.m[self.toRemove[i]])
-            self.take = self.m
-            return 1 + max(self.r0(self.take), self.r0(self.leave))
+    def r2(self, G):
+        size = len(G)
+        self.iterations += 1
+        if size != 0:
+            max = 0
+            v = 0
+            for x in G:
+                s = len(G[x])
+                if s == 0:
+                    G = self.__remove_v(x, G)
+                    return 1 + self.r2(G)
+                elif s == 2:
+                    if G[x][0] in G[G[x][1]]:
+                        G = self.__remove_v(x, G, neighbours = 1)
+                        return 1 + self.r2(G)
+                    else:
+                        neighbours = []
+                        for y in G[G[x][0]]:
+                            neighbours.append(y)
+                            G[y].append(self.n)
+                        for y in G[G[x][1]]:
+                            if y not in neighbours:
+                                neighbours.append(y)
+                                G[y].append(self.n)
+                        G[self.n] = neighbours
+                        self.n += 1
+                        G = self.__remove_v(x, G, neighbours = 1)
+                        return 1 + self.r2(G)
+                elif s == 1:
+                    G = self.__remove_v(x, G, neighbours = 1)
+                    return 1 + self.r2(G)
+                elif s > max:
+                    max = s
+                    v = x
+            G_copy = copy.deepcopy(G)
+            G1 = self.__remove_v(v, G_copy)
+            G2 = self.__remove_v(v, G, neighbours = 1)
+            res1 = self.r2(G1)
+            res2 = 1 + self.r2(G2)
+            if res1 > res2:
+                return res1
+            else:
+                return res2
         else:
-            return 0 
-        
+            return 0
         
 if __name__ == "__main__":
     dac = decreaseAndConquer(sys.argv[1])
-    res = dac()
-    print(res)
-    #x = [[1,2,3], [[2,3],[1,3], [1,2]]]
-    #print(x[1][0])
+    res,x = dac()
+    print(res, " nbr calls: ", math.log(x))
